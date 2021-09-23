91b996a0a23512bde9427f35a318aacb5a770a14
CrisCuevas84 <ccuevas84@ug.uchile.cl>
2021-09-23 20:06:01 -0300

    agregando ip a settings

--- ninjagold/settings.py
+++ ninjagold/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+mb(*3jj!9**a(=ptaz$h!zcip=jt*67(8_ap#8s^1el4htm)4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['18.220.19.99']
 
 
 # Application definition
